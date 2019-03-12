d4a68fda521a7b55f5bd246ee68fff4bdfaa356e
yipianfan <yipianfan@163.com>
2019-03-12 11:40:42 +0800

    Commit

--- src/main/java/com/boot/controller/ReadingListController.java
+++ src/main/java/com/boot/controller/ReadingListController.java
@@ -3,6 +3,8 @@ package com.boot.controller;
 import com.boot.entity.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +15,17 @@ import java.util.List;
 
 @Controller
 @RequestMapping("readingList")
+@ConfigurationProperties(prefix = "person")
 public class ReadingListController {
     private static final Logger log = LoggerFactory.getLogger(ReadingListController.class);
+    private String name;
+
+    public void setName(String name) { this.name = name; }
 
     @RequestMapping(value="/{reader}", method= RequestMethod.GET)
     public String readersBooks(@PathVariable String reader, Model model) {
         log.info("reader is: " + reader);
+        System.out.println("-->" + name);
         List<Book> readingList = null;
         if (readingList != null) {
             model.addAttribute("books", readingList);
--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -2,10 +2,10 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
         resolver.setPrefix("/WEB-INF/view/jsp/");
         resolver.setSuffix(".jsp");
         return resolver;
