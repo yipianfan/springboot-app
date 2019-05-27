026700e88af7999596ea6d376a72679e3c7d64fd
yipianfan <yipianfan@163.com>
2019-05-27 11:21:34 +0800

    删除异常类控制

--- src/main/java/com/lihai/controller/ErrorHandler.java
+++ src/main/java/com/lihai/controller/ErrorHandler.java
@@ -1,9 +1,9 @@
 package com.lihai.controller;
 
-import com.lihai.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
@@ -11,13 +11,8 @@ public class ErrorHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
     public String processValidationError(IllegalArgumentException e) {
         return "404";
-    }
-
-    @ExceptionHandler(ServiceException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String processValidationError(ServiceException e) {
-        return "500";
     }
 }
