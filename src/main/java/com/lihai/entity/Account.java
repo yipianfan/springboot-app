78781a00965d63ebbaa945c311add71dc9108498
yipianfan <yipianfan@163.com>
2019-05-31 09:20:13 +0800

    Account.java首次提交

---
+++ src/main/java/com/lihai/entity/Account.java
@@ -0,0 +1,27 @@
+package com.lihai.entity;
+
+import javax.persistence.Id;
+
+public class Account {
+    @Id
+    private String uid;
+    private String phone;
+    private String account;
+    private String password;
+
+    public String getUid() {return uid;}
+
+    public void setUid(String uid) {this.uid = uid;}
+
+    public String getPhone() {return phone;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getAccount() {return account;}
+
+    public void setAccount(String account) {this.account = account;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+}
