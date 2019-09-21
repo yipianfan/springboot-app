d6b736baf5e9a9e959298fb0073a94aae6460dd9
yipianfan <yipianfan@163.com>
2019-09-21 18:22:29 +0800

    AOP学习

--- src/main/java/com/lihai/config/LogAspect.java
+++ src/main/java/com/lihai/config/LogAspect.java
@@ -1,10 +1,19 @@
 package com.lihai.config;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 控制层日志切面
@@ -12,21 +21,27 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LogAspect {
-    private final static Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    private final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
     // "com.lihai.controller包下所有public的任意返回类型"
-    //@Pointcut("execution(public * com.lihai.controller.*.*(..))")
-    //public void log() {}
+    @Pointcut("execution(public * com.lihai.controller.*.*(..))")
+    public void log() {}
 
-    @Before("execution(public * com.lihai.controller.*.*(..))")
-    public void doBefore(JoinPoint joinPoint){
-        String methodName = joinPoint.getSignature().getName();
-        System.out.println("在" + methodName + "之前执行");
+    @Before("log()")
+    public void doBefore() {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        Map<String, String[]> paramPair = request.getParameterMap();
+        Set<Map.Entry<String, String[]>> paramSet = paramPair.entrySet();
+        Map<String, String> paramMap = new HashMap<>(paramSet.size());
+        for(Map.Entry<String, String[]> item : paramSet) {
+            paramMap.put(item.getKey(), item.getValue()[0]);
+        }
+        System.out.println();
     }
 
-    //@AfterReturning("log()")
-    @AfterReturning(returning = "obj", value = "execution(public * com.lihai.controller.*.*(..))")
-    public void doAfter(Object obj) {
-        System.out.println("====================");
+    @After("log()")
+    public void doAfter() {
+
     }
 }
